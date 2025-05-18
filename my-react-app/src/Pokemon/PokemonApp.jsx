da1438c5ea51ee0f74113f91ff1eecf71ab733c5
mqkhan125 <muhammadqasimkhan341@gmail.com>
2025-05-18 23:38:56 +0500

    update: unused vars is removed.

--- my-react-app/src/Pokemon/PokemonApp.jsx
+++ my-react-app/src/Pokemon/PokemonApp.jsx
@@ -1,4 +1,4 @@
-import React, { use, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { PokemonCards } from './PokemonCards';
 function PokemonApp() {
     const [pokemon, setPokemon] = useState([])
